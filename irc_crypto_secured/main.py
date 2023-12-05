0794a2ab4307fa43b260fe8d2270f7a29279907a
PythonHacker24 <adityapatil24680@gmail.com>
2023-12-05 14:10:09 +0530

    commit the changes

--- irc_crypto_secured/main.py
+++ irc_crypto_secured/main.py
@@ -24,100 +24,115 @@ def authenticate_channel(username, password, channel_name):
 # Endpoint for user registration
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    if request.method == "POST":
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'error': 'Username and password are required'}), 400
+        if not username or not password:
+            return jsonify({'error': 'Username and password are required'}), 400
 
-    # Check if the username is already taken
-    if any(user['username'] == username for user in users):
-        return jsonify({'error': 'Username already exists'}), 400
+        # Check if the username is already taken
+        if any(user['username'] == username for user in users):
+            return jsonify({'error': 'Username already exists'}), 400
 
-    user = {'username': username, 'password': password}
-    users.append(user)
+        user = {'username': username, 'password': password}
+        users.append(user)
 
-    return jsonify({'message': 'Registration successful'}), 201
+        return jsonify({'message': 'Registration successful'}), 201
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
 
 # Endpoint for user login
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    if request.method == "POST":
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'error': 'Username and password are required'}), 400
+        if not username or not password:
+            return jsonify({'error': 'Username and password are required'}), 400
 
-    # Check if the username and password match a registered user
-    user = next((user for user in users if user['username'] == username and user['password'] == password), None)
+        # Check if the username and password match a registered user
+        user = next((user for user in users if user['username'] == username and user['password'] == password), None)
 
-    if not user:
-        return jsonify({'error': 'Invalid username or password'}), 401
+        if not user:
+            return jsonify({'error': 'Invalid username or password'}), 401
 
-    return jsonify({'message': 'Login successful'}), 200
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
 
 # Endpoint for creating a channel
 @app.route('/create_channel', methods=['POST'])
 def create_channel():
-    data = request.json
-    channel_name = data.get('channel_name')
+    if request.method == 'POST':
+        data = request.json
+        channel_name = data.get('channel_name')
 
-    if not channel_name:
-        return jsonify({'error': 'Channel name is required'}), 400
+        if not channel_name:
+            return jsonify({'error': 'Channel name is required'}), 400
 
-    # Check if the channel name is already taken
-    if any(channel['channel_name'] == channel_name for channel in channels):
-        return jsonify({'error': 'Channel name already exists'}), 400
+        # Check if the channel name is already taken
+        if any(channel['channel_name'] == channel_name for channel in channels):
+            return jsonify({'error': 'Channel name already exists'}), 400
 
-    channel = {'channel_name': channel_name, 'users': []}
-    channels.append(channel)
+        channel = {'channel_name': channel_name, 'users': []}
+        channels.append(channel)
 
-    return jsonify({'message': 'Channel created successfully'}), 201
+        return jsonify({'message': 'Channel created successfully'}), 201
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
 
 # Endpoint for joining a channel
 @app.route('/join_channel', methods=['POST'])
 def join_channel():
-    data = request.json
-    username = data.get('username')
-    channel_name = data.get('channel_name')
+    if request.method == 'POST':
+        data = request.json
+        username = data.get('username')
+        channel_name = data.get('channel_name')
 
-    if not username or not channel_name:
-        return jsonify({'error': 'Username and channel name are required'}), 400
+        if not username or not channel_name:
+            return jsonify({'error': 'Username and channel name are required'}), 400
 
-    # Check if the user is already in the channel
-    user = next((user for user in users if user['username'] == username), None)
-    channel = next((channel for channel in channels if channel['channel_name'] == channel_name), None)
+        # Check if the user is already in the channel
+        user = next((user for user in users if user['username'] == username), None)
+        channel = next((channel for channel in channels if channel['channel_name'] == channel_name), None)
 
-    if not user or not channel:
-        return jsonify({'error': 'User or channel not found'}), 404
+        if not user or not channel:
+            return jsonify({'error': 'User or channel not found'}), 404
 
-    # Add the user to the channel
-    channel['users'].append(username)
+        # Add the user to the channel
+        channel['users'].append(username)
 
-    return jsonify({'message': f'{username} joined {channel_name}'}), 200
+        return jsonify({'message': f'{username} joined {channel_name}'}), 200
+    else:
+        return jsonify({'message': 'Method not allowed'})
 
 # Endpoint for sending a new message
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-    channel_name = data.get('channel_name')
-    content = data.get('content')
+    if request.method == 'POST':
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+        channel_name = data.get('channel_name')
+        content = data.get('content')
 
-    if not username or not password or not channel_name or not content:
-        return jsonify({'error': 'Username, password, channel name, and content are required'}), 400
+        if not username or not password or not channel_name or not content:
+            return jsonify({'error': 'Username, password, channel name, and content are required'}), 400
 
-    # Authenticate user and check if they are in the specified channel
-    if not authenticate_channel(username, password, channel_name):
-        return jsonify({'error': 'Authentication failed or user not in the channel'}), 401
+        # Authenticate user and check if they are in the specified channel
+        if not authenticate_channel(username, password, channel_name):
+            return jsonify({'error': 'Authentication failed or user not in the channel'}), 401
 
-    # Store the message
-    messages.append({'username': username, 'channel_name': channel_name, 'content': content})
+        # Store the message
+        messages.append({'username': username, 'channel_name': channel_name, 'content': content})
 
-    return jsonify({'message': 'Message sent successfully'}), 200
+        return jsonify({'message': 'Message sent successfully'}), 200
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
 
 # Endpoint for retrieving messages
 @app.route('/get_messages', methods=['GET'])
